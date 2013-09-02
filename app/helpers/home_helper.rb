66e53731f72b390b4d07b87b1e7e38940bde946c
yaniwang <yaniwang@redhat.com>
2013-09-02 16:33:59 +0800

    minor update

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,6 @@
 module HomeHelper
+  def self.get_markdown_parser
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::SmartyHTML,
+                                       :autolink => true, :space_after_headers => true)
+  end
 end
